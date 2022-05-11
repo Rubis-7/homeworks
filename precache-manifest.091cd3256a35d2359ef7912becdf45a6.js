33c60e7eeba29fd1e6d3a1f817c841e5554b0da2
Rubis-7 <zxc.zxz@yandex.ru>
2022-05-11 16:50:21 +0300

    Updates

---
+++ precache-manifest.091cd3256a35d2359ef7912becdf45a6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ae6af73044fd8a6981f80e112b25ddc",
+    "url": "/homeworks/index.html"
+  },
+  {
+    "revision": "b064f9787ffd7a271b8a",
+    "url": "/homeworks/static/css/main.e8134787.chunk.css"
+  },
+  {
+    "revision": "93ae5e67dbae7546d4d8",
+    "url": "/homeworks/static/js/2.2710960d.chunk.js"
+  },
+  {
+    "revision": "248fe9facfff898840212404b4cb88d9",
+    "url": "/homeworks/static/js/2.2710960d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b064f9787ffd7a271b8a",
+    "url": "/homeworks/static/js/main.128c0f25.chunk.js"
+  },
+  {
+    "revision": "4973bd5b858cd224b7ef",
+    "url": "/homeworks/static/js/runtime-main.047caa20.js"
+  }
+]);
